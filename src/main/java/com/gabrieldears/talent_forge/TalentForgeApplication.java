2f79939075fd97c23ce8ea2a35fd9ce5aec0d319
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-06-03 19:31:13 -0300

    feat: Initial structure
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/gabrieldears/talent_forge/TalentForgeApplication.java
@@ -0,0 +1,13 @@
+package com.gabrieldears.talent_forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabrieldears/talent_forge/TalentForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrieldears.talent_forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
